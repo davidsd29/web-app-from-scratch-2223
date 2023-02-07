20e861c2246c26c89454036b577a5d5e50de1cc3
davidsd29 <davidsdankwah@gmail.com>
2023-02-07 15:24:23 +0100

    ..

--- visitekaartje/js/API.js
+++ visitekaartje/js/API.js
@@ -5,20 +5,26 @@ const info = {
 };
 
 async function GetData() {
-    const response = await fetch('https://whois.fdnd.nl/api/v1/member?id=cldep8cyd3w3x0av0mrzat8k3');
-    const data = await response.json();
+    fetch('https://tribe-api-integration.netlify.app/.netlify/functions/members?first=200').then(res => {
+	    return res.json() })
+        .then(data => {
 
-    info.name.forEach(name => {
-        name.textContent = data.member.name + " " + data.member.surname; 
-    });
+            data.data.members.forEach(member => {
+                if (member.name == "Dave") {
 
-    info.website.forEach(url => {
-        url.textContent = data.member.webiste; 
-        url.setAttribute('href', `${data.member.webiste}`);
-    });
+                    info.name.forEach(name => {
+                        name.textContent = member.name + " " + member.surname; 
+                    });
 
-    info.gitHub.textContent = data.member.gitHubHandle;
+                    info.website.forEach(url => {
+                        url.textContent = member.webiste; 
+                        url.setAttribute('href', `${member.webiste}`);
+                    });
+
+                    info.gitHub.textContent = member.gitHubHandle; 
+                }
+            });
+        })
 }
 
 window.addEventListener('DOMContentLoaded', GetData);
-;
