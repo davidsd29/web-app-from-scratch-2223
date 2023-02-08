a8009ddec098df9441761256c24b2d6561a32ed6
davidsd29 <davidsdankwah@gmail.com>
2023-02-08 03:28:13 +0100

    Layout Changed + animations

--- visitekaartje/js/API.js
+++ visitekaartje/js/API.js
@@ -1,30 +1,29 @@
-const info = {
-    gitHub: document.getElementById('gitHub-url'),
-    website: document.querySelectorAll('.website-url'),
-    name: document.querySelectorAll('.name'),
-};
 
-async function GetData() {
+window.addEventListener('DOMContentLoaded', () => {
+
+    const info = {
+        gitHub: document.getElementById('gitHub-url'),
+        website: document.querySelectorAll('.website-url'),
+        name: document.getElementById('name'),
+    };
+
+    //Netlify = server, request on server, makes it posible to change url at will and get the data
     fetch('https://tribe-api-integration.netlify.app/.netlify/functions/members?first=200').then(res => {
 	    return res.json() })
         .then(data => {
 
+            //Loop through all the members and check if name is equal to given string
             data.data.members.forEach(member => {
                 if (member.name == "Dave") {
-
-                    info.name.forEach(name => {
-                        name.textContent = member.name + " " + member.surname; 
-                    });
-
+                    //console.log(member.id)
                     info.website.forEach(url => {
                         url.textContent = member.webiste; 
                         url.setAttribute('href', `${member.webiste}`);
                     });
-
-                    info.gitHub.textContent = member.gitHubHandle; 
+                    
+                    info.name.textContent = member.name + " " + member.surname; 
+                    info.gitHub.setAttribute('href', `${member.gitHubHandle}`);; 
                 }
             });
         })
-}
-
-window.addEventListener('DOMContentLoaded', GetData);
+});
--- visitekaartje/js/cards.js
+++ visitekaartje/js/cards.js
@@ -1,64 +1,85 @@
-window.addEventListener('DOMContentLoaded', () => {
-    let loader = document.getElementById("preloader");
-
+window.addEventListener("DOMContentLoaded", () => {
+    
     const card = {
-    home: document.querySelector('.home-page'),
-    info: document.querySelector('.info-page'),
-    contact: document.querySelector('.contact-page'),
-    skill: document.querySelector('.skill-page'),
-};
+        home: document.querySelector(".home-page"),
+        info: document.querySelector(".info-page"),
+        contact: document.querySelector(".contact-page"),
+        skill: document.querySelector(".skill-page"),
+        preloader: document.getElementById("preloader"),
+    };
 
     const button = {
-    homeBtn: document.getElementById('screen-btn'),
-    logOff: document.getElementById('logOff'),
-    nextBtn: document.getElementById('next'),
-    prevBtn: document.getElementById('prev'),
-};
+        homeBtn: document.getElementById("screen-btn"),
+        logOff: document.getElementById("logOff"),
+        nextBtn: document.getElementById("next"),
+        prevBtn: document.getElementById("prev"),
+        refresh: document.getElementById("refresh"),
+    };
 
     //Enter business card
     function LogIn() {
-        card.home.classList.add("zoomOut")
-        card.info.classList.remove("hidden")
-        card.info.classList.add("zoomIn")
+        card.home.classList.remove("float");
+        card.home.classList.add("zoomIn");
+        card.info.classList.remove("hidden");
+        card.info.classList.add("zoomOut");
         
         setTimeout(function () {
-            card.home.classList.add("hidden")
-            card.home.classList.remove("zoomOut")
-            //card.info.classList.remove("zoomIn")
+            card.home.classList.add("hidden");
+            card.home.classList.remove("zoomIn");
         }, 2500);
-
-        card.home.addEventListener('animationend', () => {
-            card.home.classList.remove("zoomOut")
-            console.log("lalal");
-        });
     }   
     
     //Close business card
     function Logout() {
-        card.info.classList.add("close")
-        card.home.classList.remove("hidden")
+        card.info.classList.add("close");
+        card.home.classList.remove("hidden");
             
         setTimeout(function () {
-            card.info.classList.add("hidden")
-            card.info.classList.remove("close")
+            card.info.classList.add("hidden");
+            card.info.classList.remove("close");
+            card.home.classList.add("float");
         }, 1500);
     }   
     
     function NextPage() {
+        if(card.contact.classList.contains("hidden")) {
+            card.skill.classList.add("hidden");
+            card.preloader.style.display = "block";
+            button.refresh.classList.add("rotate");
 
-        
-            
+            setTimeout(function () {
+                card.preloader.style.display = "none";
+                card.contact.classList.remove("hidden");
+                button.refresh.classList.remove("rotate");
+            }, 1000);
+        } else PrevPage();
     }   
     
     function PrevPage() {
-            
-  
-    }
+        if(card.skill.classList.contains("hidden")) {
+            card.contact.classList.add("hidden");
+            card.preloader.style.display = "block";
+            button.refresh.classList.add("rotate");
 
-    function Loader(){
-        //preloader 
+            setTimeout(function () {
+                card.preloader.style.display = "none";
+                card.skill.classList.remove("hidden");
+                button.refresh.classList.remove("rotate");
+            }, 1000);
+        } else NextPage();
+    }    
+    
+    function RefreshPage() {
+        card.skill.style.display = "none";
+        card.contact.style.display = "none";
+        card.preloader.style.display = "block";
+        button.refresh.classList.add("rotate");
+
         setTimeout(function () {
-            loader.style.display = "none";
+            card.skill.style.display = "block";
+            card.contact.style.display = "block";
+            card.preloader.style.display = "none";
+            button.refresh.classList.remove("rotate");
         }, 1000);
     }
 
@@ -66,6 +87,7 @@ window.addEventListener('DOMContentLoaded', () => {
     button.logOff.addEventListener("click", Logout);
     button.nextBtn.addEventListener("click", NextPage);
     button.prevBtn.addEventListener("click", PrevPage);
+    button.refresh.addEventListener("click", RefreshPage);
 
-    console.log('DOM fully loaded and parsed');
+    console.log("DOM fully loaded and parsed");
 });
