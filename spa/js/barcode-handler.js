c25ac8b20f0263c6e6ee21970247f43879d395bd
davidsd29 <davidsdankwah@gmail.com>
2023-02-28 15:51:59 +0100

    added Uploading from gallery

--- spa/js/barcode-handler.js
+++ spa/js/barcode-handler.js
@@ -1,15 +1,13 @@
-import {RenderProduct} from './render-details.js';
-import {GetData} from './API/fetch-product.js';
-
 const scanner = new Html5Qrcode("scanner");
 const stopScanningBtn = document.getElementById("stop-camera-scan");
+const fileCodeReader = new Html5Qrcode("reader");
 
-export function StartScan () {
+// Start scanning of the camera
+function StartCameraScan () {
     const config = { fps: 10, qrbox: { width: 250, height: 250 } };
     const qrCodeSuccessCallback = (barcode) => {
-        // GetData(barcode);
         // // return data; 
-        StopScanning();
+        StopCameraScan();
         window.location.hash = `#product/${barcode}`;
         GetRouter();
     };
@@ -19,8 +17,8 @@ export function StartScan () {
     });
 }
 
-// Stops scanning of the
-export function StopScanning() {
+// Stops scanning of the camera
+function StopCameraScan() {
     scanner.stop().then((ignore) => {
         // QR Code scanning is stopped.
         // Clears scanning instance. Stops the camera
@@ -35,3 +33,27 @@ export function StopScanning() {
         console.log("Stop function faild, please try again");
     });
 }
+
+function GetFileBarcode(event) {
+    if (event.target.files.length == 0) {
+        console.log("no file found");
+        return;
+    }
+
+    const imageFile = event.target.files[0];
+
+    // Scan QR Code
+    fileCodeReader.scanFile(imageFile, true)
+    .then(barcode => {
+    // barcode succes = true
+    window.location.hash = `#product/${barcode}`;
+    console.log(barcode);
+    
+    })
+    .catch(err => {
+    // failure, handle it.
+    console.log(`Error scanning file. Reason: ${err}`)
+    });
+}
+
+export { StartCameraScan, StopCameraScan, GetFileBarcode };
